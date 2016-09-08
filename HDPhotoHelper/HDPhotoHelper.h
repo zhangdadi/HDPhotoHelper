43bd905b6986909a8b3899bbf280d771989c79ef
张达棣 <z_dadi@163.com>
2016-09-08 10:07:28 +0800

    调整目录

--- HDPhotoHelper/HDPhotoHelper.h
+++ HDPhotoHelper/HDPhotoHelper.h
@@ -5,6 +5,27 @@
 //  Created by 张达棣 on 16/7/7.
 //  Copyright © 2016年 HD. All rights reserved.
 //
+//  GitHub:https://github.com/zhangdadi/HDPhotoHelper
+//  email: z_dadi@163.com
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
 
 #import <UIKit/UIKit.h>
 
@@ -35,7 +56,5 @@ typedef void (^HDPhotoHelperBlock) (id data);
  *  @param selectImageBlock 选择照片后回调
  */
 - (void)showWithSelectImageBlock:(HDPhotoHelperBlock)selectImageBlock;
-
-@property(nonatomic, weak) id<UINavigationControllerDelegate, UIImagePickerControllerDelegate> delegate NS_DEPRECATED(2_0, 2_0, 2_0, 2_0, "不可使用delegate");
 
 @end
--- HDPhotoHelper/HDPhotoHelper.m
+++ HDPhotoHelper/HDPhotoHelper.m
@@ -5,6 +5,27 @@
 //  Created by 张达棣 on 16/7/7.
 //  Copyright © 2016年 HD. All rights reserved.
 //
+//  GitHub:https://github.com/zhangdadi/HDPhotoHelper
+//  email: z_dadi@163.com
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
 
 #import "HDPhotoHelper.h"
 #import <MobileCoreServices/MobileCoreServices.h>
@@ -12,6 +33,7 @@
 
 @interface HDPhotoDelegateHelper: NSObject<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 @property (nonatomic, copy) HDPhotoHelperBlock selectImageBlock;
+
 @end
 
 @interface HDPhotoHelper ()
@@ -60,13 +82,12 @@
     }else if ([mediaType isEqualToString:(NSString *)kUTTypeMovie]){
         //获取视频文件的url
         NSURL* mediaURL = [info objectForKey:UIImagePickerControllerMediaURL];
-        ALAssetsLibrary *assetsLibrary = [[ALAssetsLibrary alloc] init];
         if (_selectImageBlock) {
             _selectImageBlock(mediaURL);
         }
     }
     
-    [picker dismissModalViewControllerAnimated:YES];
+    [picker dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
